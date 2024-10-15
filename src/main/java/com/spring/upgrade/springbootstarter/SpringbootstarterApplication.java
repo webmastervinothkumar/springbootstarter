7d12ae40dea3e4ac2363a22be065c7e326234ca1
webmastervino <webmastervinothkumar@gmail.com>
2024-10-14 20:59:53 -0400

    setup

---
+++ src/main/java/com/spring/upgrade/springbootstarter/SpringbootstarterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.upgrade.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/upgrade/springbootstarter/SpringbootstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.upgrade.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
